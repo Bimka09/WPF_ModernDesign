883d637ff9c2e5c933ddc9a3adf1bde46ec2b146
Bimka09 <90Alex09@mail.ru>
2023-04-16 22:35:30 +0300

    Create

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using ModernDesign.Core;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    class MainViewModel:ObservableObject
+    {
+		public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public HomeViewModel homeVM { get; set; }
+		public DiscoveryViewModel discoveryVM { get; set; }
+
+		private object _currentView;
+
+		public object CurrentView
+        {
+			get { return _currentView; }
+			set 
+			{ 
+				_currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+        public MainViewModel()
+        {
+			homeVM = new HomeViewModel();
+			discoveryVM = new DiscoveryViewModel();
+			CurrentView = homeVM;
+
+
+            HomeViewCommand = new RelayCommand(o =>
+			{
+				CurrentView = homeVM;
+			});
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = discoveryVM;
+            });
+        }
+
+
+	}
+}
